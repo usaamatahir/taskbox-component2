a62d9ad4589e715915af72e32f3f129589fd0736
usaamatahir <usamatahir8330@gmail.com>
2020-11-02 05:00:11 -0800

    First Commit

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,14 +1,4 @@
 module.exports = {
-
-  typescript: {
-    check: false,
-    checkOptions: {},
-    reactDocgen: 'react-docgen-typescript',
-    reactDocgenTypescriptOptions: {
-      shouldExtractLiteralValuesFromEnum: true,
-      propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
-    },
-  },
   "stories": [
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
